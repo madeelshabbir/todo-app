57e5a7940b72af7894a64c62719c66e3fbfb122b
Adeel Shabbir <madeelshabbirs@gmail.com>
2023-01-04 13:07:39 +0500

    Hello world html response

--- src/main/java/com/dev/springboot/todoapp/hello/HelloController.java
+++ src/main/java/com/dev/springboot/todoapp/hello/HelloController.java
@@ -12,4 +12,20 @@ public class HelloController {
   public String say() {
     return "Hello World!";
   }
+
+
+  @RequestMapping("hello/say-in-html")
+  @ResponseBody
+  public String sayInHtml() {
+    StringBuffer buffer = new StringBuffer();
+    buffer.append("<html>");
+    buffer.append("<head>");
+    buffer.append("<title>Hello Page</title>");
+    buffer.append("</head>");
+    buffer.append("<body>");
+    buffer.append("<p>Hello World!</p>");
+    buffer.append("</body>");
+    buffer.append("</html>");
+    return buffer.toString();
+  }
 }
