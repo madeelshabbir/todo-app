6f64db21f907023c33ed48a95269d41b702670ee
Adeel Shabbir <madeelshabbirs@gmail.com>
2023-01-14 01:44:08 -0600

    Query params

--- src/main/java/com/dev/springboot/todoapp/login/LoginController.java
+++ src/main/java/com/dev/springboot/todoapp/login/LoginController.java
@@ -1,12 +1,15 @@
 package com.dev.springboot.todoapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
   @RequestMapping("login")
-  public String show() {
+  public String show(@RequestParam String name, ModelMap model) {
+    model.put("name", name);
     return "login";
   }
 }
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -3,6 +3,6 @@
     <title>Login Page</title>
   </head>
   <body>
-    <p>Welcome to the login page</p>
+    <p>Welcome to the login ${name}</p>
   </body>
 </html>
