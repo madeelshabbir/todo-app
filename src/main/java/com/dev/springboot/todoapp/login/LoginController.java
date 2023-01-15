fa5b339d8db0f978ccf53b45dd7d127ac6d98c7b
Adeel Shabbir <madeelshabbirs@gmail.com>
2023-01-15 05:26:28 -0600

    Logger

--- src/main/java/com/dev/springboot/todoapp/login/LoginController.java
+++ src/main/java/com/dev/springboot/todoapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.dev.springboot.todoapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +9,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+  private Logger logger = (Logger) LoggerFactory.getLogger(getClass());
+
   @RequestMapping("login")
   public String show(@RequestParam String name, ModelMap model) {
     model.put("name", name);
+    logger.info("Request params is {}", name);
+    logger.debug("Request params is {}", name);
+    logger.warn("Request params is {}", name);
     return "login";
   }
 }
